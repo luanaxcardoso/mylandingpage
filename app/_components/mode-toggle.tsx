173fa767b79b035b1d077b69ce00913b2b14f50a
luanaxcardoso <luanacardoso.bc@gmail.com>
2024-05-26 18:34:41 -0300

    atualização da página 'sobre'

--- app/_components/mode-toggle.tsx
+++ app/_components/mode-toggle.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button} from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import {
     DropdownMenu,
     DropdownMenuItem,
@@ -9,29 +9,30 @@ import {
 } from '@/components/ui/dropdown-menu';
 
 import { useTheme } from 'next-themes';
-import { Moon, Sun} from 'lucide-react';
+import { Moon, Sun } from 'lucide-react';
 
-export function ModeToggle(){
+export function ModeToggle() {
     const { setTheme } = useTheme();
 
     return (
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-            <Button variant='ghost' size='lg' className='h-6 w-6 px-0 ml-2'>
-                <Sun className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' size={24} />
-                <Moon className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' size={24} />
-                <span className='sr-only'>Toggle</span>
-            </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-            <DropdownMenuItem onClick={() => setTheme('light')}>
-                Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme('dark')}>
-                Dark
-            </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    )
+        <div className="absolute top-0 right-0 m-4">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant='ghost' size='sm' className='relative h-6 w-6 p-0'>
+                        <Sun className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' size={24} />
+                        <Moon className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' size={24} />
+                        <span className='sr-only'>Toggle</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem onClick={() => setTheme('light')}>
+                        Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme('dark')}>
+                        Dark
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
+    );
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,20 @@ import { ModeToggle } from "./_components/mode-toggle";
 
 export default function Home() {
   return (
-    <main className="">
-      <section id="sobre" className="space-y-6 py-8 md:py12 lg:py32">
-        <div className="container flex max-w-[64rem] flex-col items-center text-center gap-4">
-          <span>
-                <ModeToggle />
-          </span>
+    <main className="relative">  
+      <div className="absolute top-0 right-0 m-4"> 
+        <ModeToggle />
+      </div>
+      <section id="sobre" className="space-y-6 py-4 md:py-12 lg:py-28">
+  <div className="container flex max-w-[64rem] flex-col items-center text-center gap-4">
+  <h1 className="text-primary text-4xl font-bold text-center">Luana Cardoso</h1>
+    <span className="rounded-2xl bg-muted px-6 py-1 text-lg font-semibold flex items-center">
+      <p>Desenvolvedora Full-Stack Júnior</p>
+    </span>
+  </div>
+  
+</section>
 
-        </div>
-        <h1 className="text-primary text-4xl font-bold text-center">Luana Cardoso</h1>        
-      </section>
-      
     </main>
   );
 }
